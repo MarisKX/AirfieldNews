686b42800f2249328c040f99ccfcb1ff0936d102
MarisKX <maris.kurpnieks@gmail.com>
2022-03-02 20:58:48 +0000

    Cleaned settings (moved to env variables)

--- airfieldnews/settings.py
+++ airfieldnews/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-egk#9@(k_(%(ki6i8^wv30dckw9s^x33w^whgs)$*^+0#z#p$-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
