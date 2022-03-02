3615928e3503642d7930e644cbf0950f7988285e
MarisKX <maris.kurpnieks@gmail.com>
2022-03-02 20:40:32 +0000

    Connected to Heroku and postgresDB

--- airfieldnews/settings.py
+++ airfieldnews/settings.py
@@ -128,18 +128,19 @@ WSGI_APPLICATION = 'airfieldnews.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
-
-#DATABASES = {
-#    'default': dj_database_url.parse("postgres://qhzoweszdkwlrp:f5a12404b7ec461f4e89cc6351efad3268ae775caf7156723dd367c0fd5b912a@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/d5l4p37t4kgrv0")
-#}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
