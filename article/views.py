a39f6a42b3ff12c35873755608a67cc53375064f
MarisKX <maris.kurpnieks@gmail.com>
2022-03-13 17:49:58 +0000

    Created search and filter functionality, improved laytout for mobile devices

--- article/views.py
+++ article/views.py
@@ -9,12 +9,14 @@ def article(request, article_number):
     article = get_object_or_404(Article, article_number=article_number)
     article_elements = ArticleElements.objects.filter(article_number=article).order_by('sequence')
     article_tags = ArticleTags.objects.filter(article_number=article).order_by('tag_name')
+    tags = TagsAll.objects.all().order_by('display_name')
 
     context = {
         'article': article,
         'article_elements': article_elements,
         'categories': categories,
-        'article_tags': article_tags
+        'article_tags': article_tags,
+        'tags': tags
     }
 
     return render(request, 'article/article.html', context)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from article.models import (
@@ -18,20 +18,45 @@ def index(request):
     latest_articles = Article.objects.all().order_by('-date')[:6]
     categories = Category.objects.all()
     tags = TagsAll.objects.all().order_by('display_name')
-    regions = Regions.objects.all()
+    regions = Regions.objects.all().order_by('display_name')
 
     query_filter_category = None
+    query_filter_region = None
+    query_filter_tag = None
+    query_search_all = None
 
     latest_articles_by_category = []
 
     for category in categories:
-        article_by_category = Article.objects.filter(article_category=category).order_by('-date')[:4]
+        article_by_category = Article.objects.filter(article_category=category).order_by('-date')[:6]
         latest_articles_by_category.append(article_by_category)
 
     if latest_articles.count() >= 4:
 
         if request.GET:
             # Handles main filtering functionality
+            if 'q' in request.GET:
+                query_search_all = request.GET['q']
+                if not query_search_all:
+                    # messages.error(request, "You didn't enter any search criteria!")
+                    return redirect(reverse('home'))
+                
+                queries = Q(
+                    heading__icontains=query_search_all) | Q(
+                    article_insight__icontains=query_search_all) | Q(
+                    article_elements__article_element_content_text__icontains=query_search_all) | Q(
+                    article_tags__tag_name__name__icontains=query_search_all)
+                all_articles = all_articles.filter(queries).distinct()
+
+                context = {
+                        'query_search_all': query_search_all,
+                        'all_articles': all_articles,
+                        'categories': categories,
+                        'regions': regions,
+                        'tags': tags,
+                    }
+                return render(request, 'categories/search-results.html', context)
+
             if 'category' in request.GET:
                 query_filter_category = request.GET['category']
 
@@ -49,6 +74,42 @@ def index(request):
                     'tags': tags,
                 }
                 return render(request, 'categories/categories.html', context)
+
+            if 'region' in request.GET:
+                query_filter_region = request.GET['region']
+
+                queries = Q(
+                    article_region__name__icontains=query_filter_region)
+                all_articles = all_articles.filter(queries)
+                selected_region = get_object_or_404(Regions, name=query_filter_region)
+
+                context = {
+                    'query_filter_region': query_filter_region,
+                    'all_articles': all_articles,
+                    'selected_category': selected_region,
+                    'categories': categories,
+                    'regions': regions,
+                    'tags': tags,
+                }
+                return render(request, 'categories/regions.html', context)
+
+            if 'tag' in request.GET:
+                query_filter_tag = request.GET['tag']
+
+                queries = Q(
+                    article_tags__tag_name__name__icontains=query_filter_tag)
+                all_articles = all_articles.filter(queries).distinct()
+                selected_tag = get_object_or_404(TagsAll, name=query_filter_tag)
+
+                context = {
+                    'query_filter_region': query_filter_region,
+                    'all_articles': all_articles,
+                    'selected_tag': selected_tag.tag_name,
+                    'categories': categories,
+                    'regions': regions,
+                    'tags': tags,
+                }
+                return render(request, 'categories/tags.html', context)
 
         context = {
             'latest_articles': latest_articles,
